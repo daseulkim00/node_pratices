c140ef4c9ad3ef4ae897c9b5dbcdd736f52d0042
daseulkim00 <ektmf8619@naver.com>
2021-11-18 14:49:59 +0900

    logging

--- mysite/index.js
+++ mysite/index.js
@@ -21,7 +21,7 @@ dotenv.config({
 const{ applicationRouter } = require('./routes')
 
 //3. Logger
-
+const logger = require('./logging');
 
 //Application Setup
 const application = express()
@@ -43,16 +43,16 @@ applicationRouter.setup(application);
 //6. Server Setup
 http.createServer(application)
     .on('listening', function(){
-        console.info(`http server runs on ${process.env.PORT}`);
+        logger.info(`http server runs on ${process.env.PORT}`);
     })
     .on('error', function(error){
         switch(error.code) {
             case 'EACCESS':
-                console.error(`${process.env.PORT} requires privileges`);
+                logger.error(`${process.env.PORT} requires privileges`);
                 process.exit(1);
                 break;
             case 'EADDRINUSE':
-                console.error(`${process.env.PORT} is already in use`);
+                logger.error(`${process.env.PORT} is already in use`);
                 process.exit(1);
                 break;
             default:
